411e0b829645b329e9cb143f9c9ac53f7734662b
hikariakarui121 <80142147+hikariakarui121@users.noreply.github.com>
2021-09-05 11:53:51 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function(){
   $('.nav-btn').on('click',function(){
       $(this).toggleClass("-active");
-      $('#nav').toggleClass("-active");
+      $('.navigation').toggleClass("-active");
   })
 });
 
