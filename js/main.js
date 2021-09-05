43db56dc05a47e3e4c8ef40a6238f4dd25a19a00
hikariakarui121 <80142147+hikariakarui121@users.noreply.github.com>
2021-09-05 11:22:07 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+  $('.nav-btn').on('click',function(){
+      $(this).toggleClass("-active");
+      $('#nav').toggleClass("-active");
+  })
+});
+
+// スクロール
+$('a[href^="#"]').click(function(){
+  var speed = 500;
+  var href= $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top -60;
+  $("html, body").animate({scrollTop:position}, speed, "swing");
+  return false;
+});
